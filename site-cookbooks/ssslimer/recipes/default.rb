67aa92f07cbc77d07aea1a48e72f2e5b451aeb6a
Seiei Higa <hanachin@gmail.com>
2013-09-17 19:28:55 +0900

    slimerjsの前のリバースプロキシ用にnginxをインストール

--- site-cookbooks/ssslimer/recipes/default.rb
+++ site-cookbooks/ssslimer/recipes/default.rb
@@ -6,10 +6,21 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
 git '/usr/local/ssslimer' do
    repository 'https://github.com/hanachin/ssslimer.git'
    revision   's3upload'
    action     :sync
    user       'root'
    group      'root'
+end
+
+include_recipe "nginx::source"
+
+template "#{node['nginx']['dir']}/conf.d/ssslimer.conf" do
+  source 'ssslimer.conf.erb'
+  mode   0644
+  owner  'root'
+  group  'root'
+  notifies :restart, 'service[nginx]'
 end
