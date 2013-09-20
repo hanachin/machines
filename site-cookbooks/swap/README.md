775a55f6ad9206e53aaaa831240661faf6f2c690
Seiei Higa <hanachin@gmail.com>
2013-09-20 16:44:28 +0900

    Add swap recipe

---
+++ site-cookbooks/swap/README.md
@@ -0,0 +1,1 @@
+Create swap file. The original recipe is [here](http://qiita.com/naoya@github/items/2059e3755962e907315e)
---
+++ site-cookbooks/swap/metadata.rb
@@ -0,0 +1,7 @@
+name             'swap'
+maintainer       'hanachin'
+maintainer_email 'hanachin@gmail.com'
+license          'All rights reserved'
+description      'Create swap file. original is http://qiita.com/naoya@github/items/2059e3755962e907315e'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
